944c5aa97062914827e3091f085d4c60c4c02520
mbhardwaj327 <65974231+mbhardwaj327@users.noreply.github.com>
2021-03-31 00:24:27 +0530

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,22 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=0
+z=0
+def add_cal(a,b):
+
+    if a>0 and b>0:
+        c=a+b
+
+    return c
+
+def multiply(a,b):
+    c=a*b
+
+def divide(a,b):
+    c=a/b
+
+def substract(a,b):
+    c=abs(a-b)
+
+z=add_cal(a,b)
+print(z)
